8f2d2dff5ac9471dd4fc37ab6dd97d2c5e193913
Abhay Chauhan <abhaychauhan@Abhays-MacBook-Air.local>
2024-12-11 18:03:49 +0530

    final-commit

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "Job-Portal",
+    })
+    .then(() => {
+      console.log("Connected to database.");
+    })
+    .catch((err) => {
+      console.log(`Some Error occured. ${err}`);
+      console.log("DB URI:", process.env.MONGO_URI);
+    });
+};
